8551acc48a6d1880bd523d40b222f7f59d9a685b
HarshKumar07986 <121846286+HarshKumar07986@users.noreply.github.com>
2024-06-23 10:41:45 +0000

    Conversion of data types

---
+++ 01/basics/datatypesconversion.js
@@ -0,0 +1,21 @@
+let score="Harsh"
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let num=Number(score)
+
+//console.log(typeof num)
+//console.log( num)
+
+//"11" => 11
+//"abc"=> NaN
+//true=> 1 && false=>0
+
+let logged=1
+let blogged=Boolean(logged)
+console.log(typeof blogged)
+console.log(blogged)
+
+//1=> true && 0=> false
+//""=> false
+//"Harsh"=> True
