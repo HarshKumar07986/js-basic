d22fcdffbff9df0c2888fa1a68f19da4b38441a7
HarshKumar07986 <121846286+HarshKumar07986@users.noreply.github.com>
2024-06-28 09:01:24 +0000

    datatypes_summary

---
+++ 01/basics/02/datatypes_Summary.js
@@ -0,0 +1,31 @@
+
+// Primitive Data Types
+
+// 7 Types: String , Number , Boolean , null , Undefined , Symbol , Bigint
+
+const score=100
+const isLoggedIn=false
+const outsideTemp=null
+
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+//console.log(id==anotherId)
+
+// Reference Type (Non-Primitive)
+
+// Array, Object , Functions => return type object
+
+const cricketers=['Virat','Dhoni','Rohit']
+ let myObj= {
+    name: "Harsh",
+    age: 21
+ }
+
+ console.log(myObj)
+ const myfunc= function(){
+    console.log("Hello World")
+ }
+
