84d27e613e7c953f570c744c4bc5ec1e8607fae7
HarshKumar07986 <121846286+HarshKumar07986@users.noreply.github.com>
2024-07-02 15:57:54 +0000

    numbers

---
+++ number.js
@@ -0,0 +1,21 @@
+const score=100
+//console.log(score)
+
+const balance= new Number(100)
+//console.log(balance)
+
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(2))
+
+const oth=123.989
+console.log(oth.toPrecision(3))
+
+const num=1000000
+console.log(num.toLocaleString())
+
+//for indian 'en-In'
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+ 
+ 
