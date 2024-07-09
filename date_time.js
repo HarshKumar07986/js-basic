fb8ee5cae76bbfece780753ec78fab54783e3dcf
HarshKumar07986 <121846286+HarshKumar07986@users.noreply.github.com>
2024-07-09 08:11:18 +0000

    datetime

---
+++ date_time.js
@@ -0,0 +1,12 @@
+//dates
+
+let myDate=new Date()
+//console.log(myDate.toString())
+//console.log(myDate.toDateString())
+//console.log(myDate.toLocaleString())
+//console.log(typeof myDate)
+
+let createdDate=new Date(2023,0,23)
+console.log(createdDate.toString())
+
+ 
