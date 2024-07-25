6ef9385758f014121d4993908757f2f89e7ed2db
HarshKumar07986 <121846286+HarshKumar07986@users.noreply.github.com>
2024-07-25 12:38:10 +0000

    scopes

---
+++ scopes.js
@@ -0,0 +1,40 @@
+//let a=10
+//const b=20
+//var c=30
+ let a=63
+if(true)
+{
+    let a=10
+    const b=20
+    var c=30
+    //console.log(a)
+}
+//console.log(a)
+//console.log(b)
+//console.log(c)
+
+function one() {
+    const username="Harsh"
+    function two() {
+        const website="youtube"
+        console.log(username);
+    }
+    //console.log(website);
+    two()
+}
+one()
+
+if (true) {
+    const user="harsh"
+    if (user=="harsh") {
+        const web="youtube"
+        console.log(user+web);
+
+    }
+    //console.log(web);
+    
+}
+//console.log(user);
+
+
+
