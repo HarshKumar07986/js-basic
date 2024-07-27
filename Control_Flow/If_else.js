40199c04e0bfa7c7ec409322bee736d8e017921e
HarshKumar07986 <121846286+HarshKumar07986@users.noreply.github.com>
2024-07-27 16:22:01 +0000

    If_else

---
+++ Control_Flow/If_else.js
@@ -0,0 +1,63 @@
+const a=true
+
+if (2==="2") {
+//    console.log("execute");
+}
+
+//>,<,==,!=,>=,=<,===
+
+const score=200
+
+if (score>100) {
+    var power=10
+   // console.log(`${power} ,is executed`);
+}
+//console.log(`${power} ,is executed`);
+
+const balance=1000
+
+//if (balance>500) console.log("test") , console.log("test2");
+
+if (balance<500) {
+   // console.log("less than 500");
+}
+else if (balance<700) {
+   // console.log("less than 700");
+} else {
+   // console.log("less than 1200");
+    
+}
+
+const b=true
+const c=true
+
+if (b && c && 2==3) {
+  //  console.log("both are true");
+}
+//like this "or" operator if one is false though it will run
+ const month=3
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar"); 
+        break;   
+    default:
+        break;
+}
+
+//In switch key string value can also be used
+
+//falsy values
+
+//false,0,-0,null,"",undefined,NaN
+
+//truely values
+
+//"0",'false'," ",[],{},function(){}
+
+
