b4f278b223ddaa20adfaa22e5fa6fce6c7b64131
HarshKumar07986 <121846286+HarshKumar07986@users.noreply.github.com>
2024-07-30 19:54:03 +0000

    while

---
+++ Iterations/while.js
@@ -0,0 +1,23 @@
+//while
+
+let index=0
+while (index<=10) {
+  //  console.log(`value of${index}`);
+    index++
+}
+
+let myArray=['a','b','c']
+let ar=0
+while (ar<myArray.length) {
+    console.log(ar);
+    ar++
+}
+
+//do-while
+
+let score=1
+
+do {
+    console.log(score);
+    score++
+} while (score<=10);
